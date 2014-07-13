0ac2240cc97f07d74cf2f303099d5ccd758488c9
Walter Landry <wlandry@caltech.edu>
2014-07-13 15:01:50 -0700

    Reformat

--- src/Query/callback.cxx
+++ src/Query/callback.cxx
@@ -6,16 +6,20 @@ namespace tinyhtm
   {
     int64_t count;
     enum htm_errcode ec;
-    if(type==Type::circle) {
-      count=htm_tree_s2circle(&(tree.tree), &(center.v3), r, &ec, fn);
-    }
+    if(type==Type::circle)
+      {
+        count=htm_tree_s2circle(&(tree.tree), &(center.v3), r, &ec, fn);
+      }
     else if(type==Type::ellipse)
-      count=htm_tree_s2ellipse(&(tree.tree), &(ellipse.ellipse), &ec, fn);
+      {
+        count=htm_tree_s2ellipse(&(tree.tree), &(ellipse.ellipse), &ec, fn);
+      }
     else if(type==Type::polygon)
-      count=htm_tree_s2cpoly(&(tree.tree), poly, &ec, fn);
+      {
+        count=htm_tree_s2cpoly(&(tree.tree), poly, &ec, fn);
+      }
     else
       throw Exception("Bad tinyhtm::Query::Type");
-
 
     return count;
   }
